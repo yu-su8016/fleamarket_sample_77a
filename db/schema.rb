db23d07ceb6be7eb9288cca52144de55ff111ff0
Sakamoto Takaaki <st.sari66@gmail.com>
2020-06-22 16:02:11 +0900

    Fix
    categoryテーブルからandcestry削除、マイグレーションファイルの順番を修正

--- db/schema.rb
+++ db/schema.rb
@@ -25,10 +25,8 @@ ActiveRecord::Schema.define(version: 2020_06_19_110307) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["ancestry"], name: "index_categories_on_ancestry"
     t.index ["name"], name: "index_categories_on_name"
   end
 
