48de523b294cd33fe0c1a3bc88d3c5590d869184
Sakamoto Takaaki <st.sari66@gmail.com>
2020-06-22 20:16:36 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  root "items#index"
+  root "users#show"
+  resources :items do
+    collection do
+      get :purchase
+    end
+  end
+
+  resources :users do
+    collection do
+      get :logout
+    end
+  end
+  resources :cards do
+  end
 end
