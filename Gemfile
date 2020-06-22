06086bf2a8ed5286d66d606cfb6e7403261caca0
hide8811 <ikuyedih18@gmail.com>
2020-06-22 17:50:05 +0900

    Add: ancestryの追加
    Categoryテーブルのレコードをツリー構造として編成するため。

--- Gemfile
+++ Gemfile
@@ -83,6 +83,7 @@ gem 'fog-aws'
 gem 'mini_magick'
 gem 'active_hash'
 gem 'rails-i18n'
+gem 'ancestry'
 
 group :production do
   gem 'unicorn'
--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2020_06_29_123554) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
     t.index ["name"], name: "index_categories_on_name"
   end
 
