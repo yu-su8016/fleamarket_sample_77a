6acb47d0ba43a8121d4ff1f2ecf86c55d48c28ca
Sakamoto Takaaki <st.sari66@gmail.com>
2020-06-22 11:01:37 +0900

    Update 20200619101222_create_cards.rb

--- db/migrate/20200619101222_create_cards.rb
+++ db/migrate/20200619101222_create_cards.rb
@@ -2,7 +2,8 @@ class CreateCards < ActiveRecord::Migration[5.2]
   def change
     create_table :cards do |t|
       t.string :unmber,         null: false
-      t.string :limit,          null: false
+      t.string :limit_month,    null: false
+      t.string :limit_year,     null: false
       t.string :security_code,  null: false
       t.references :user, type: :bigint, null: false
       t.timestamps
