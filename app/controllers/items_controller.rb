b5e7c7634c8bf7e776f2513a0f655916913e1804
ngsw1031 <nagasawa12051031@gmail.com>
2020-06-29 16:51:08 +0900

    item_show setting

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,37 @@
 class ItemsController < ApplicationController
+  # before_action :move_to_index, except: [:index, :show, :search]
+  
   def index
+    @items = Item.all
   end
-  def purchase
-    
+
+  def new
+    @item = Item.new
+    @image = @item.images.build
+  end
+
+  def show
+    @item = Item.find(params[:id]) 
+    @category = Category.find(@item.category_id)
+    @comment = Comment.new
+    @comments = @item.comments.includes(:user)
+    @likes = Like.new
+    # @likes = @item.likes.includes(:user)
+  end
+  
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy 
+      # if items.user_id == current_user.id
+      #   item.destroy 
+      # end
+  end
+
+  def delete
+  end
+  
+  private
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,17 @@
 class Item < ApplicationRecord
   has_many :likes
   has_many :comments
-  has_many :images
-  belongs_to :user
+  has_many :images, dependent: :destroy
+  belongs_to :user, foreign_key: 'seller_id'
   belongs_to :category
+
+  def previous
+    Item.where("id < ?", self.id).order("id DESC").first
+  end
+
+  def next
+    Item.where("id > ?", self.id).order("id ASC").first
+  end
+ 
+  accepts_nested_attributes_for :images, allow_destroy: true
 end
