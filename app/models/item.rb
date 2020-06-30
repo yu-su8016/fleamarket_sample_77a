e2a443d905dc53ea78fb0087b4850fc6ea0c57fc
hide8811 <ikuyedih18@gmail.com>
2020-06-30 10:32:29 +0900

    Add: active_hashの使用

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,15 @@ class Item < ApplicationRecord
   belongs_to :user, foreign_key: "seller_id"
   belongs_to :category, foreign_key: "category_id"
 
-  accepts_nested_attributes_for :images
-
   validates :name, presence: true, length: { maximum: 40 }
   validates :explanation, presence: true, length: { maximum: 1000 }
-  validates :condition, :delivery_fee, :prefecture, :day, :delivery_method, presence: true
+  validates :condition_id, :delivery_fee_id, :prefecture_id, :day_id, :delivery_method_id, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :day
+  belongs_to_active_hash :delivery_method
 end
