b4437434ba140dacc112aa40e899a34cbc484ab0
hide8811 <ikuyedih18@gmail.com>
2020-06-30 10:36:12 +0900

    Add: itemテーブルへの保存機能の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,39 @@
 class ItemsController < ApplicationController
+
+  before_action :set_hash, only: [:new, :create]
   def index
   end
   def purchase
     
+  end
+
+  def show
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to "/"
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def set_hash
+    @conditions = Condition.all
+    @delivery_fees = DeliveryFee.all
+    @delivery_methods = DeliveryMethod.all
+    @prefectures = Prefecture.all
+    @days = Day.all
+  end
+
+  def item_params
+    params.permit(:name, :explanation, :category_id, :brand, :size, :condition_id, :delivery_fee_id, :prefecture_id, :day_id, :delivery_method_id, :price).merge(seller_id: 1)
   end
 end
