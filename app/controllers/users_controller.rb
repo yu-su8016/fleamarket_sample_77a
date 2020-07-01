070c5e6297f0b2a061e77ad43161bb6d3bfbb26d
naowakao <65644019+naowakao@users.noreply.github.com>
2020-07-01 14:33:05 +0900

    新規登録、ログイン【修正２】

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,6 @@ class UsersController < ApplicationController
 
 
   def create
-    @user = User.new(user_params)
-    @user.save
-    
-    redirect_to root_path
-
-    
   end
 
   def edit
@@ -35,13 +29,5 @@ class UsersController < ApplicationController
   def logout
   end
 
-  private
 
-  def user_params
-    params.require(:user).permit(:email, :password, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday, destinations_attributes:[:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :prefecture, :city, :address, :after_address, :phone])
-  end
-
-  def destinations_params
-    params.require(:destinations).permit(destinations:[:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :prefecture, :city, :address, :after_address, :phone])
-  end
 end
