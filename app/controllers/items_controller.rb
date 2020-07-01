41ce837d8143a5eb9f579c745f06a8d417f4a30d
Sakamoto Takaaki <st.sari66@gmail.com>
2020-07-01 19:15:02 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,16 @@
 class ItemsController < ApplicationController
   def index
   end
-  def purchase
-    
+  def show
+    @user = User.find(params[:user_id])
+    require 'payjp'
+    Payjp.api_key = Rails.application.credentials.PAYJP[:secret_access_key]
+    ## ユーザー登録機能実装後修正 find()をcurrent_user.idへ
+    if @user.card
+      customer = Payjp::Customer.retrieve(@user.card.customer_id)
+      @cards = customer.cards.data[0].id
+    end
+    destinations = Destination.includes(:user).where(users: {id: params[:user_id]})
+    @destinations = destinations[0]
   end
 end
