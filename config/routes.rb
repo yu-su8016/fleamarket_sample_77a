e97247187e4479569da2e669a7508d9c6f867ff9
Sakamoto Takaaki <st.sari66@gmail.com>
2020-07-04 21:15:59 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,17 @@ Rails.application.routes.draw do
     collection do
       get :logout
     end
-  end
-  resources :cards do
+    resources :items do
+      resources :purchases do
+        collection do
+          get :purchase, :after_purchase
+        end
+      end
+    end
+    resources :cards do
+      collection do
+        get :delete
+      end
+    end
   end
 end
