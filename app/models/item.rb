8436ae83bc4d5b53163a89c4b767b7dcb5834c0a
Sakamoto Takaaki <st.sari66@gmail.com>
2020-07-04 18:30:41 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,6 @@ class Item < ApplicationRecord
   has_many :likes
   has_many :comments
   has_many :images
-  belongs_to :user
+  belongs_to :user, :foreign_key => 'seller_id'
   belongs_to :category
 end
