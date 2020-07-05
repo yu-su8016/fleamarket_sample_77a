ce1818c664ad770a73c00bbd6d7f5f28af0f30d0
Sakamoto Takaaki <st.sari66@gmail.com>
2020-07-06 00:47:34 +0900

    Update rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require_relative "./support/auth_helper"
+require_relative "./support/controller_macros"
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
