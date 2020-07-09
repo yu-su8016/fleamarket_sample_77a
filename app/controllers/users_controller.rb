e0ddba0cf8b899cf9517dfdcef3f47a89025328b
ngsw1031 <nagasawa12051031@gmail.com>
2020-07-09 18:42:15 +0900

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,10 @@
 class UsersController < ApplicationController
   before_action :header_category_parent, only: [:show, :logout, :like]
   
-  def index
-    
-  end
-
-  def new
-    @user = User.new
-    @user.build_destinations
-  end
-
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
   def show
-
   end
 
   def logout
-
   end
 
   def like
